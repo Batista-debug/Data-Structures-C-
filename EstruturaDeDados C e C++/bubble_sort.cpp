255d7188a8bdf563abd16315693f0c60f7ab4bae
My Life Codar <65630879+Batista-debug@users.noreply.github.com>
2020-05-23 12:30:59 -0300

    Arquivo Revisado #1

---
+++ EstruturaDeDados C e C++/bubble_sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#define TAM 10
+
+using namespace std;
+
+void imprimeVetor(int vetor[]){
+    int i;
+    cout << "\n";
+    for(i = 0; i < TAM; i++){
+        cout << " |" << vetor[i] << "| ";
+    }
+}
+
+void bubble_sort(int vetor[TAM]){
+
+    int x, y, aux;
+
+    for(x = 0; x < TAM; x++){
+
+        for(y = x + 1; y < TAM; y++){
+
+            imprimeVetor(vetor);
+
+            if(vetor[x] > vetor[y]){
+                aux = vetor[x];
+                vetor[x] = vetor[y];
+                vetor[y] = aux;
+            }
+
+        }
+
+    }
+
+}
+
+int main(){
+
+    int vetor[TAM] = {10,9,8,7,6,5,4,3,2,1};
+
+    bubble_sort(vetor);
+
+    imprimeVetor(vetor);
+
+    return 0;
+}
